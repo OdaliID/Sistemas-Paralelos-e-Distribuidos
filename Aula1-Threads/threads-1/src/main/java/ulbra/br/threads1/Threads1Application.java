a9aee1dd5a58397b2c063e19c222dedf013f9c17
Odali Araújo de Sousa Junior <62185010+OdaliID@users.noreply.github.com>
2023-02-11 16:17:19 -0300

    Create Threads1Application.java

---
+++ Aula1-Threads/threads-1/src/main/java/ulbra/br/threads1/Threads1Application.java
@@ -0,0 +1,13 @@
+package ulbra.br.threads1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Threads1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Threads1Application.class, args);
+	}
+
+}
