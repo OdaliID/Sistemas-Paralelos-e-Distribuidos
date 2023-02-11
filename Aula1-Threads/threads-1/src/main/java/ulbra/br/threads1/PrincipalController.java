aa0cc5e9b45ba88a9d1950e227d155a0cd978f96
Odali Araújo de Sousa Junior <62185010+OdaliID@users.noreply.github.com>
2023-02-11 16:17:17 -0300

    Create PrincipalController.java

---
+++ Aula1-Threads/threads-1/src/main/java/ulbra/br/threads1/PrincipalController.java
@@ -0,0 +1,27 @@
+package ulbra.br.threads1;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class PrincipalController {
+
+        @GetMapping
+        public String semThread() throws InterruptedException{
+            TarefaDemorada tarefa = new TarefaDemorada();
+            tarefa.processar();
+            return "Executou...";
+        }
+        
+       
+
+        @GetMapping("thread")
+        public String comThread() throws InterruptedException{
+            TarefaDemorada2 tarefa = new TarefaDemorada2();
+            Thread minhaThread = new Thread(tarefa);
+            minhaThread.start();
+            return "Executou...";
+        }
+}
+http://localhost:8080/thread
