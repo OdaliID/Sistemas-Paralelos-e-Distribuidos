3e2216c85d2e6381ff7e41b93fcccfa33329a5ef
OdaliID <62185010+OdaliID@users.noreply.github.com>
2023-03-01 20:16:44 -0300

    Update App.java

--- Aula3- Atividade/src/App.java
+++ Aula3- Atividade/src/App.java
@@ -12,6 +12,6 @@ public class App {
             threads[i].join();
         }
         
-        System.out.println("Valor final: " + contador.getNumero());
+        System.out.println("Valor final: " + contador.getNumero()); // Exibe o valor final do contador
     }
 }
