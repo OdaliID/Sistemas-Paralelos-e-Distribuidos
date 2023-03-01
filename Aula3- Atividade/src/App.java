73dedf1f6eca6ac6b70f3f15d7121728b2e1ab4b
OdaliID <62185010+OdaliID@users.noreply.github.com>
2023-03-01 20:14:24 -0300

    Update App.java

--- Aula3- Atividade/src/App.java
+++ Aula3- Atividade/src/App.java
@@ -4,12 +4,12 @@ public class App {
         Thread[] threads = new Thread[1000];  // Cria um array de threads
         
         for (int i = 0; i < threads.length; i++) {  // Cria e inicia as threads
-            threads[i] = new Thread(new Tarefa(contador));
-            threads[i].start();
+            threads[i] = new Thread(new Tarefa(contador));  // Cada thread recebe uma instância do contador via construtor
+            threads[i].start(); 
         }
         
         for (int i = 0; i < threads.length; i++) {
-            threads[i].join();
+            threads[i].join();  
         }
         
         System.out.println("Valor final: " + contador.getNumero());
