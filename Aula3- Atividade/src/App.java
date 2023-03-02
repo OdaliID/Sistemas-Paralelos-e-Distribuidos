16e3dab424cadac755104915fd033ccad7b77757
OdaliID <62185010+OdaliID@users.noreply.github.com>
2023-03-01 21:10:07 -0300

    Update App.java

--- Aula3- Atividade/src/App.java
+++ Aula3- Atividade/src/App.java
@@ -1,3 +1,33 @@
+class Contador {
+    private int numero;
+    
+    public synchronized void incrementar() {
+        this.numero++;
+    }
+    
+    public synchronized int getNumero() {
+        return this.numero;
+    }
+}
+
+class Tarefa implements Runnable {
+    private Contador contador;
+    
+    public Tarefa(Contador contador) {
+        this.contador = contador;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(100);
+            contador.incrementar();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
 public class App {
     public static void main(String[] args) throws InterruptedException {
         Contador contador = new Contador();
