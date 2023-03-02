b5761d48a0e28d483860f819475a507e31f6d37e
OdaliID <62185010+OdaliID@users.noreply.github.com>
2023-03-01 21:04:22 -0300

    Update App.java

--- Aula2-Atividade/src/App.java
+++ Aula2-Atividade/src/App.java
@@ -1,4 +1,8 @@
 public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         
         String texto = "License";
